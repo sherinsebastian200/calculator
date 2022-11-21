fb2f9333ff9711f9d9932dfdc4be03ccd5875933
sherin200 <sherinsebastian21>
2022-11-21 10:37:02 +0530

    update

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter second number");
+        int num2 = sc.nextInt();
+        System.out.println("Enter operation");
+        int operation = sc.nextInt();
+        System.out.println("Enter 1 Addition. 2.substraction 3.multiplication 4.division");
+
+        switch(operation)
+        {
+            }
+        }
+
 }
